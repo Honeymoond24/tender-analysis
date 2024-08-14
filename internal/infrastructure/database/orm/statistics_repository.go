df1b2d1a9dbaeaaa7abef460d494a695c64f8917
Daniyal Zhanturin <daniyal5536@gmail.com>
2024-08-14 12:54:23 +0500

    feature: use pointer in repository

--- internal/infrastructure/database/orm/statistics_repository.go
+++ internal/infrastructure/database/orm/statistics_repository.go
@@ -6,17 +6,17 @@ type StatisticsRepository struct {
 	DB *gorm.DB
 }
 
-func (s StatisticsRepository) MostActiveCategoryByTenders() (string, int) {
+func (s *StatisticsRepository) MostActiveCategoryByTenders() (string, int) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (s StatisticsRepository) MostActiveCategoryByPriceSum() (string, float64) {
+func (s *StatisticsRepository) MostActiveCategoryByPriceSum() (string, float64) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (s StatisticsRepository) ActiveTenders() int64 {
+func (s *StatisticsRepository) ActiveTenders() int64 {
 	var count int64
 	s.DB.Model(&Tender{}).Count(&count)
 	return count

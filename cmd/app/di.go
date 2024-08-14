5a2396f2009483e558b1be5dfbe2e1fae190fa72
Daniyal Zhanturin <daniyal5536@gmail.com>
2024-08-14 12:56:18 +0500

    feature: restructure entrypoint

---
+++ cmd/app/di.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"git.b4i.kz/b4ikz/tenderok-analytics/cmd/app/config"
+	"git.b4i.kz/b4ikz/tenderok-analytics/internal/infrastructure/presentation"
+	"git.b4i.kz/b4ikz/tenderok-analytics/internal/infrastructure/presentation/router"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+	"go.uber.org/zap"
+	"net/http"
+)
+
+func AsRoute(f any) any {
+	return fx.Annotate(
+		f,
+		fx.As(new(router.Route)),
+		fx.ResultTags(`group:"routes"`),
+	)
+}
+
+func GetFxOptions() []fx.Option {
+	return []fx.Option{
+		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+			return &fxevent.ZapLogger{Logger: log}
+		}),
+		fx.Provide(
+			presentation.NewHTTPServer, // http server
+			fx.Annotate(
+				router.NewServeMux,
+				fx.ParamTags(`group:"routes"`),
+			), // http serve mux with routes
+			AsRoute(router.NewStatisticsHandler),
+			AsRoute(router.NewPersonalStatisticsHandler),
+			presentation.SetupServerHandler,
+			zap.NewProduction, // logger
+			config.GetHTTPServerPort,
+		),
+		fx.Invoke(
+			func(*http.Server) {},
+		),
+	}
+}
--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,49 +1,9 @@
 package main
 
 import (
-	"git.b4i.kz/b4ikz/tenderok-analytics/internal/infrastructure/presentation"
 	"go.uber.org/fx"
-	"go.uber.org/fx/fxevent"
-	"go.uber.org/zap"
-	"net/http"
 )
 
-func AsRoute(f any) any {
-	return fx.Annotate(
-		f,
-		fx.As(new(presentation.Route)),
-		fx.ResultTags(`group:"routes"`),
-	)
-}
-
 func main() {
-	//err := godotenv.Load()
-	//port, err := strconv.Atoi(os.Getenv("API_PORT"))
-	//apiPrefix := os.Getenv("API_PREFIX")
-	//log.Println("Read environment variables")
-
-	//err = presentation.StartServer(port, apiPrefix)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	fx.New(
-		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
-			return &fxevent.ZapLogger{Logger: log}
-		}),
-		fx.Provide(
-			presentation.NewHTTPServer, // http server
-			fx.Annotate(
-				presentation.NewServeMux,
-				fx.ParamTags(`group:"routes"`),
-			), // http serve mux with routes
-			AsRoute(presentation.NewRootHandler),
-			AsRoute(presentation.NewStatisticsHandler),
-			presentation.SetupServerHandler,
-			zap.NewProduction, // logger
-		),
-		fx.Invoke(
-			func(*http.Server) {},
-		),
-	).Run()
+	fx.New(GetFxOptions()...).Run()
 }

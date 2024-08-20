6d6bdd922521e4244a8ce93680a864be3fcfb7ca
Daniyal Zhanturin <daniyal5536@gmail.com>
2024-08-20 14:55:01 +0500

    refactor: remove unused variables

--- internal/application/repository.go
+++ internal/application/repository.go
@@ -1,21 +1,5 @@
 package application
 
-import (
-	"git.b4i.kz/b4ikz/tenderok-analytics/internal/domain"
-)
-
-type UserRepository interface {
-	GetUserByID(id uint) (*domain.User, error)
-}
-
-type TenderRepository interface {
-	GetOne(id int) (*domain.Tender, error)
-}
-
-type CompanyRepository interface {
-	GetOne(id int) (*domain.Company, error)
-}
-
 type StatisticsRepository interface {
 	ActiveTenders() int64
 	MostActiveCategoryByTenders() (string, int)
